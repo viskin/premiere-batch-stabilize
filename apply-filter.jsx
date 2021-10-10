31ea2bdfdd0979006605459ebbd34e039d1d6fde
viskin <r.viskin@terikon.com>
2021-10-10 22:34:07 +0300

    applies effect and sets values

--- apply-filter.jsx
+++ apply-filter.jsx
@@ -3,7 +3,12 @@
 // qe.reflect.methods
 // qe.reflect.properties
 
-var presetName = "Warp Stabilizer 20%" // Can be effect / preset name
+//https://lesterbanks.com/2019/04/how-to-add-effects-in-premiere-with-extendscript/
+
+//https://github.com/qmasingarbe/pymiere/blob/master/example_and_documentation.md - how to export
+//https://github.com/yearbook/yearbook.github.io/blob/master/esdocs/source/Premiere/classes/QEProject.json - parameters
+
+var effectName = "Warp Stabilizer" // Sadly cannot be a preset name
 
 app.enableQE()
 
@@ -25,14 +30,24 @@ for (var i = 0; i < projectItems.length; i++) {
         var qsequence = qe.project.getSequenceAt(sequenceIndex)
         var qclip = qsequence.getVideoTrackAt(0).getItemAt(0)
 
-        //qe.project.getVideoEffectList()
-        var effect = qe.project.getVideoEffectByName(presetName)
-        app.trace(effect.name)
-        qclip.addVideoEffect(effect)
+        var effectComponent = getComponentByDisplayName(components, effectName)
+
+        if (!effectComponent) { // Add effect it not exists
+            //qe.project.getVideoEffectList()
+            var effect = qe.project.getVideoEffectByName(effectName)
+            if (!effect.name) throw "Effect now found: " + effectName;
+            app.trace(effect.name)
+            qclip.addVideoEffect(effect)
+
+            effectComponent = getComponentByDisplayName(components, effectName)
+        }
+
+        applyEffectProperties(effectComponent)
 
         // TODO: wait for 
-        //sequence.isDoneAnalyzingForVideoEffects()
+        var done = sequence.isDoneAnalyzingForVideoEffects()
         
+        var a = 1;
     }
 
 }
@@ -53,4 +68,28 @@ function projectItemToSequenceIndex(projectItem) {
         }
     }
     return null;
+}
+
+function getComponentByDisplayName(components, name) {
+    for (var i = 0; i < components.length; i++) {
+        var component = components[i];
+        if (name === component.displayName) {
+            return component;
+        }
+    }
+    return null;
+}
+
+function applyEffectProperties(component) {
+    for (var i = 0; i < component.properties.length; i++) {
+        var property = component.properties[i];
+        switch (property.displayName) {
+            case "Smoothness":
+                property.setValue(9, 1); // 1 means update gui
+                break;
+            case "Crop Less <-> Smooth More":
+                property.setValue(10, 1); // 1 means update gui
+                break;
+        }
+    }
 }
